c0b35fbb3efdc6a514f144042498a11999feed09
killian-coder <chintalima49@gmail.com>
2018-12-14 17:36:05 +0200

    Bookstore pharmacy update

--- bookstore.php
+++ bookstore.php
@@ -13,39 +13,196 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
      <!-- Navbar -->
-    <nav class="Dark-blue  blue darken-4" role="navigation">
-    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Library</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="#">Home</a></li>
-      </ul>
-
-      <ul id="nav-mobile" class="sidenav">
-        <li><a href="#">Profile</a></li>
-      </ul>
-      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-    </div>
-  </nav>
-  <br><br><br><br>
-<div class="row">
-    <div class="col s12 m2">
-      <div class="card small">
-        <div class="card-image">
-          <img src="https://5.imimg.com/data5/SR/TQ/GLADMIN-2954845/pharmacy-objective-type-book-500x500.png"  width="500" height="200"/>
-          <span class="card-title">Card Title</span>
-          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+  <div class="role">
+        <nav class="Dark-blue  blue darken-4" role="navigation">
+        <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Library</a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="#">Home</a></li>
+          </ul>
+          <ul id="nav-mobile" class="sidenav">
+            <li><a href="#">Profile</a></li>
+          </ul>
+          <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
         </div>
-        <div class="card-content">
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div class="card-action">
-          <a href="#">This is a link</a>
-        </div>
-      </div>
+      </nav>
   </div>
+
+  <!-- HEADER  -->
+  
+  <div id="container">
+  <div class="row">
+    <div class="col s12 m12 l2">
+      <nav class="search-title-filter">
+        <div class="nav-wrapper">
+          <form>
+            <div class="input-field">
+              <input id="search" type="search" required>
+              <label for="search"><i class="material-icons">search</i></label>
+              <i class="material-icons">close</i>
+            </div>
+          </form>
+        </div>
+      </nav>
+</div>
+  <header>
+    <h4>Pharmacy Books</h4>
+</header>
+</div>
+  <!-- content  -->
+<div class="row">
+    <div class="col s12 m4 l2">
+      <p>
+      <ul id="slide-out" class="sidenav">
+    <li><div class="user-view">
+      <div class="background">
+        <img src="https://materializecss.com/images/office.jpg">
+      </div>
+      <a href="#user"><img class="circle" src="https://materializecss.com/images/yuna.jpg"></a>
+      <a href="#name"><span class="white-text name">Juliet Tembo</span></a>
+      <a href="#email"><span class="white-text email">jtembo@gmail.com</span></a>
+    </div></li>
+    <li><a href="#!"><i class="material-icons">cloud</i>See your Saved Books</a></li>
+    <li><a href="#!">Second Link</a></li>
+    <li><div class="divider"></div></li>
+    <li><a class="subheader">Subheader</a></li>
+    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+  </ul>
+  <a href="#" data-target="slide-out" class="sidenav-trigger btn-floating btn-small waves-effect waves-light blue"><i class="material-icons">menu</i></a>
+
+  <!-- OTHER STUFF  ON THE Side Bar -->
+  <div class="row">
+  <ul class="collapsible">
+    <li>
+      <div class="collapsible-header"><i class="material-icons">filter_drama</i>Available Books</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+  </ul>
+        
+</div>
+      </p>
+    </div>
+    <div class="col s12 m6 l8">
+      <p>
+        <!-- first image -->
+        <div class="col s12 m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="https://5.imimg.com/data5/SR/TQ/GLADMIN-2954845/pharmacy-objective-type-book-500x500.png" height="200px">
+            <span class="card-title"></span>
+            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+          </div>
+          <div class="card-content">
+            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+          </div>
+        </div>
+        <p>Pharmacy Book 2018 ed </p> 
+      </div>
+      
+      <!-- Second image  -->
+      <div class="col s12 m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="https://media.karousell.com/media/photos/products/2018/04/09/pharmacy_books_bundle_1523243227_600d4ffc.jpg" height="200">
+            <span class="card-title"></span>
+            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+          </div>
+          <div class="card-content">
+            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+          </div>
+        </div>
+        <p>Pharmacy Book 2018 ed </p>
+
+      </div>
+      <!-- third image  -->
+      <div class="col s12 m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="https://5.imimg.com/data5/SR/TQ/GLADMIN-2954845/pharmacy-objective-type-book-500x500.png" height="200">
+            <span class="card-title"></span>
+            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+          </div>
+          <div class="card-content">
+            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+          </div>
+        </div>
+        <p>Pharmacy Book 2018 ed </p>
+
+      </div>
+      <!-- FOURTH IMAGE  -->
+
+      <div class="col s12 m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="https://media.karousell.com/media/photos/products/2018/04/09/pharmacy_books_bundle_1523243227_48a0e2d8.jpg" height="200">
+            <span class="card-title"></span>
+            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+          </div>
+          <div class="card-content">
+            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+          </div>
+        </div>
+        <p><h6>Pharmacy Book 2018 ed </h6></p>
+
+      </div>
+
+
+      </p>
+  </div>
+  
+  </div>
+
 
     <body>
       <!--JavaScript at end of body for optimized loading-->
+      <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+      
+<script> 
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  // Initialize collapsible  dropdown variation
+  // var collapsibleElem = document.querySelector('.collapsible');
+  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+</script>
     </body>
   </html>

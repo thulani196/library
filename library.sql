16f551a4a13f6d17c9434cd661c3cc1b832693e9
thulani196 <tembothulani96@outlook.com>
2018-12-18 00:02:34 +0200

    Updated database

--- library.sql
+++ library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 11, 2018 at 10:45 PM
+-- Generation Time: Dec 17, 2018 at 11:01 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
@@ -21,6 +21,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `library`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `title` varchar(70) NOT NULL,
+  `description` varchar(160) NOT NULL,
+  `book` text NOT NULL,
+  `thumbnail` text NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=armscii8;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `title`, `description`, `book`, `thumbnail`, `course_id`, `user_id`, `created_at`) VALUES
+(15, 'Hey', 'qwery', '47b904049fbef51c63ea7e70b20dac7e.pdf', '903adc95b12403bc7de89431509be49e.png', 0, 1, '2018-12-17 21:00:18');
 
 -- --------------------------------------------------------
 
@@ -45,12 +69,18 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `password`, `mobile_number`, `student_number`, `created_at`, `nrc_id`, `role`) VALUES
-(1, 'Thulani', 'Tembo', '$2y$10$A8B2bkx0UUDHVjXNGfDuCue3t3xesygfRccB2vKWir79x.0j0x.6W', '0976245430', '15090046', '2018-12-11 20:19:15', '339899/10/1', 0),
+(1, 'Thulani', 'Tembo', '$2y$10$A8B2bkx0UUDHVjXNGfDuCue3t3xesygfRccB2vKWir79x.0j0x.6W', '0976245430', '15090046', '2018-12-11 20:19:15', '339899/10/1', 1),
 (2, 'Aggrey', 'Chintalima', '$2y$10$79fjv6VSDgDBkddE5D/1M.Na9vOh6MfnS7N7qGakQ5o2ZDWP4DPCW', '0976245430', '15090049', '2018-12-11 20:19:53', '339899/10/1', 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -61,6 +91,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `users`
